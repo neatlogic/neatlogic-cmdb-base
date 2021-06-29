6aa92f9119e514628aaae54fec71e9187e53e90b
陈其炜 <chenqw@techsure.com.cn>
2021-06-29 18:51:31 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/framework/cmdb/exception/cientity/CiEntityIsInUsedException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.exception.cientity;
+
+import codedriver.framework.cmdb.dto.ci.AttrVo;
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CiEntityIsInUsedException extends ApiRuntimeException {
+    public CiEntityIsInUsedException(List<AttrVo> attrList) {
+        super("当前配置项已经被以下属性引用：" + attrList.stream().map(attr -> attr.getLabel() + "(" + attr.getCiLabel() + ")").collect(Collectors.joining(",")));
+    }
+}
--- src/main/java/codedriver/framework/cmdb/exception/cientity/CiEntityIsLockedException.java
+++ src/main/java/codedriver/framework/cmdb/exception/cientity/CiEntityIsLockedException.java
@@ -7,10 +7,8 @@ package codedriver.framework.cmdb.exception.cientity;
 
 import codedriver.framework.exception.core.ApiRuntimeException;
 
-@SuppressWarnings("serial")
 public class CiEntityIsLockedException extends ApiRuntimeException {
     public CiEntityIsLockedException(Long id) {
         super("配置项：" + id + " 已被锁定编辑");
     }
-
 }
