734344df1b8e295968884445c01184a94bb095de
陈其炜 <chenqw@techsure.com.cn>
2021-07-30 15:45:28 +0800

    --bug=1008439 --user=陈其炜 配置模型-模型下拉框属性已经被配置项引用，下拉框属性的目标模型可修改并保存 https://www.tapd.cn/54247054/s/1167716
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/framework/cmdb/exception/attr/AttrIsUsedInTargetException.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.exception.attr;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+public class AttrIsUsedInTargetException extends ApiRuntimeException {
+    public AttrIsUsedInTargetException() {
+        super("当前属性已存在数据，不能更改目标模型");
+    }
+}
