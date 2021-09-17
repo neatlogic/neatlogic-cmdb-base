a0593b1de0deb0632e0f55d7b34b476ed2f051b0
吕佐康 <lvzk@techsure.com.cn>
2021-09-17 15:45:41 +0800

    update autoexec

--- src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterResourceHasRepeatAccountException.java
+++ src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterResourceHasRepeatAccountException.java
@@ -11,11 +11,11 @@ import codedriver.framework.exception.core.ApiRuntimeException;
  * @author lvzk
  * @since 2021/7/27 10:34
  **/
-public class ResourceCenterResourceHasRepeatAccount extends ApiRuntimeException {
+public class ResourceCenterResourceHasRepeatAccountException extends ApiRuntimeException {
 
     private static final long serialVersionUID = 6934793951984585148L;
 
-    public ResourceCenterResourceHasRepeatAccount(String resourceId, String account, String protocol) {
+    public ResourceCenterResourceHasRepeatAccountException(String resourceId, String account, String protocol) {
         super("资源\""+ resourceId+"）\"存在多个 协议为“"+protocol+"”用户名为“"+account+"”的账号,请联系管理员");
     }
 }
