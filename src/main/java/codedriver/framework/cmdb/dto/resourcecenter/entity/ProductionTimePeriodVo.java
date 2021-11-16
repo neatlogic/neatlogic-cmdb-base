b6050fe3ac3b5a8f55ad54d98a942be394681d0a
林邦泉 <linbq@techsure.com.cn>
2021-11-16 19:46:24 +0800

    --task=1005505 --user=林邦泉 后端-新增resource_appsystem_productiontimeperiod视图 https://www.tapd.cn/54247054/s/1188434

--- src/main/java/codedriver/framework/cmdb/dto/resourcecenter/entity/ProductionTimePeriodVo.java
+++ src/main/java/codedriver/framework/cmdb/dto/resourcecenter/entity/ProductionTimePeriodVo.java
@@ -21,13 +21,13 @@ public class ProductionTimePeriodVo {
     @EntityField(name = "资源id", type = ApiParamType.LONG)
     @ResourceField(name = "resource_id")
     private Long resourceId;
-    @EntityField(name = "资源id", type = ApiParamType.LONG)
+    @EntityField(name = "投产时段id", type = ApiParamType.LONG)
     @ResourceField(name = "productiontimeperiod_id")
     private Long productionTimePeriodId;
-    @EntityField(name = "编码", type = ApiParamType.LONG)
+    @EntityField(name = "投产时段编码", type = ApiParamType.LONG)
     @ResourceField(name = "productiontimeperiod_code")
     private String productionTimePeriodCode;
-    @EntityField(name = "名称", type = ApiParamType.LONG)
+    @EntityField(name = "投产时段名称", type = ApiParamType.LONG)
     @ResourceField(name = "productiontimeperiod_label")
     private String productionTimePeriodLabel;
 
