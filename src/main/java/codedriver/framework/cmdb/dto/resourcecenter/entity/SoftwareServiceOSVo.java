62623f11638227b735a5d2e20b2d681631239e6e
林邦泉 <linbq@techsure.com.cn>
2021-12-07 18:47:06 +0800

    --story=1005633 --user=林邦泉 应用清单-增加四个字段 https://www.tapd.cn/54247054/s/1193163

---
+++ src/main/java/codedriver/framework/cmdb/dto/resourcecenter/entity/SoftwareServiceOSVo.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.dto.resourcecenter.entity;
+
+import codedriver.framework.cmdb.annotation.ResourceField;
+import codedriver.framework.cmdb.annotation.ResourceType;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
+@ResourceType(name = "resource_softwareservice_os", label = "软件服务与操作系统关系")
+public class SoftwareServiceOSVo {
+    @EntityField(name = "操作系统id", type = ApiParamType.LONG)
+    @ResourceField(name = "os_id")
+    private Long osId;
+    @EntityField(name = "操作系统名称", type = ApiParamType.STRING)
+    @ResourceField(name = "os_name")
+    private String osName;
+    @EntityField(name = "操作系统类型id", type = ApiParamType.LONG)
+    @ResourceField(name = "type_id")
+    private Long typeId;
+    @EntityField(name = "资源id", type = ApiParamType.LONG)
+    @ResourceField(name = "resource_id")
+    private Long resourceId;
+
+    public Long getOsId() {
+        return osId;
+    }
+
+    public void setOsId(Long osId) {
+        this.osId = osId;
+    }
+
+    public String getOsName() {
+        return osName;
+    }
+
+    public void setOsName(String osName) {
+        this.osName = osName;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+}
