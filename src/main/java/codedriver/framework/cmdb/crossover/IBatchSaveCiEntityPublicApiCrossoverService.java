834cc38fefb4162e8b92efb076e13d05316424a1
lvzk <lvzk@techsure.com.cn>
2021-12-27 11:31:29 +0800

    暴露public 批量插入接口

---
+++ src/main/java/codedriver/framework/cmdb/crossover/IBatchSaveCiEntityPublicApiCrossoverService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.crossover;
+
+import codedriver.framework.crossover.ICrossoverService;
+import com.alibaba.fastjson.JSONObject;
+
+public interface IBatchSaveCiEntityPublicApiCrossoverService extends ICrossoverService {
+
+    Object myDoService(JSONObject jsonObj) throws Exception;
+
+}
