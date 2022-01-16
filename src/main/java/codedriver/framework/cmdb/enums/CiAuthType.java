51590a588a0898463c8efc9c242a9c42402901f0
陈其炜 <chenqw@techsure.com.cn>
2022-01-16 10:21:53 +0800

    增加修改事务回滚
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/cmdb/enums/CiAuthType.java
+++ src/main/java/codedriver/framework/cmdb/enums/CiAuthType.java
@@ -1,13 +1,15 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.framework.cmdb.enums;
 
 public enum CiAuthType {
-    CIENTITYINSERT("cientityinsert", "配置项添加"), CIENTITYUPDATE("cientityupdate", "配置项更新"),
-    CIENTITYDELETE("cientitydelete", "配置项删除"), CIENTITYQUERY("cientityquery", "配置项查询"),
+    CIENTITYINSERT("cientityinsert", "配置项添加"),
+    CIENTITYUPDATE("cientityupdate", "配置项更新"),
+    CIENTITYDELETE("cientitydelete", "配置项删除"),
+    CIENTITYQUERY("cientityquery", "配置项查询"),
     CIMANAGE("cimanage", "模型管理"),
     TRANSACTIONMANAGE("transactionmanage", "事务管理"),
     CIENTITYRECOVER("cientityrecover", "配置项恢复"),
@@ -19,6 +21,15 @@ public enum CiAuthType {
     CiAuthType(String _value, String _text) {
         this.value = _value;
         this.text = _text;
+    }
+
+    public static CiAuthType get(String _value) {
+        for (CiAuthType s : CiAuthType.values()) {
+            if (s.getValue().equals(_value)) {
+                return s;
+            }
+        }
+        return null;
     }
 
     public String getValue() {
