49ade1299b3a0b0bd3a1c02d517b3e5004ead1a7
longrf <techsure901>
2022-02-16 16:34:52 +0800

    账号管理的account（用户名）改为非必填，非tagent账号需检测account必填

--- src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterAccountNotCreateTagentAccountException.java
+++ src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterAccountNotCreateTagentAccountException.java
@@ -2,8 +2,8 @@ package codedriver.framework.cmdb.exception.resourcecenter;
 
 import codedriver.framework.exception.core.ApiRuntimeException;
 
-public class ResourceCenterAccountNotCreateTagentAccount extends ApiRuntimeException {
-    public ResourceCenterAccountNotCreateTagentAccount() {
+public class ResourceCenterAccountNotCreateTagentAccountException extends ApiRuntimeException {
+    public ResourceCenterAccountNotCreateTagentAccountException() {
         super("使用tagent协议的账号不支持手动添加");
     }
 }
--- src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterAccountProtocolNotFoundException.java
+++ src/main/java/codedriver/framework/cmdb/exception/resourcecenter/ResourceCenterAccountProtocolNotFoundException.java
@@ -4,7 +4,12 @@ import codedriver.framework.exception.core.ApiRuntimeException;
 
 public class ResourceCenterAccountProtocolNotFoundException extends ApiRuntimeException {
     private static final long serialVersionUID = -5055024535330898128L;
+
     public ResourceCenterAccountProtocolNotFoundException(Long id) {
-        super("协议："+ id+"不存在");
+        super("协议：" + id + "不存在");
+    }
+
+    public ResourceCenterAccountProtocolNotFoundException() {
+        super("协议不存在");
     }
 }
