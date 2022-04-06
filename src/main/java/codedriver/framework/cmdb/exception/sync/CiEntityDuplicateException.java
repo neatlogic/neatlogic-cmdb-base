796f333623181146945c52232ad9ac5dfc72bc84
陈其炜 <chenqw@techsure.com.cn>
2022-04-06 12:05:44 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/cmdb/exception/sync/CiEntityDuplicateException.java
+++ src/main/java/codedriver/framework/cmdb/exception/sync/CiEntityDuplicateException.java
@@ -5,16 +5,20 @@
 
 package codedriver.framework.cmdb.exception.sync;
 
+import codedriver.framework.cmdb.dto.cientity.CiEntityVo;
 import codedriver.framework.exception.core.ApiRuntimeException;
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.stream.Collectors;
 
 public class CiEntityDuplicateException extends ApiRuntimeException {
     public CiEntityDuplicateException() {
         super("找到多个配置项，无法更新或添加");
     }
 
-    public CiEntityDuplicateException(JSONObject dataObj) {
-        super("找到多个配置项，无法更新或添加，原始数据：" + dataObj.toString());
+    public CiEntityDuplicateException(CiEntityVo attrConditionVo, JSONObject dataObj) {
+        super("唯一规则：" + attrConditionVo.getAttrFilterList().stream().map(d -> d.getLabel() + "(" + d.getName() + ") " + d.getExpressionName() + " " + String.join(",", d.getValueList()))
+                .collect(Collectors.joining(" 且 ")) + "在模型" + attrConditionVo.getCiLabel() + "(" + attrConditionVo.getCiName() + ")" + "中找到多个配置项，无法更新或添加，原始数据：" + dataObj.toString());
     }
 
 }
