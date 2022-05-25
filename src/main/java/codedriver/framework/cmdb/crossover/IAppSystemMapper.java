8c94c9cfcb22c4df9358e2db3c8a83b57f1a7378
lvzk <lvzk@techsure.com.cn>
2022-05-25 15:42:40 +0800

    --task=1006367 --user=lvzk 应用配置-授权相关接口 https://www.tapd.cn/54247054/s/1230632

---
+++ src/main/java/codedriver/framework/cmdb/crossover/IAppSystemMapper.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.crossover;
+
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppEnvironmentVo;
+import codedriver.framework.crossover.ICrossoverService;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface IAppSystemMapper extends ICrossoverService {
+    List<AppEnvironmentVo> getAppEnvironmentListByAppResourceIdAndModuleResourceIdList(@Param("appResourceId") Long appResourceId, @Param("moduleResourceIdList") List<Long> moduleIdList,@Param("schemaName") String schemaName);
+}
