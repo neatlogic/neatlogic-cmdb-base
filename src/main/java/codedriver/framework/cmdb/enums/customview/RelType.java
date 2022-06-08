7dcdd111365eac639211c822d777bcfe1786312a
陈其炜 <chenqw@techsure.com.cn>
2022-06-08 11:29:08 +0800

    --bug=1009730 --user=陈其炜 创建自定义视图报错 https://www.tapd.cn/54247054/s/1233618
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/cmdb/enums/customview/RelType.java
+++ src/main/java/codedriver/framework/cmdb/enums/customview/RelType.java
@@ -1,13 +1,13 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.framework.cmdb.enums.customview;
 
 public enum RelType {
-    ATTR("attr", "属性"), CI("ci", "模型"), REL("rel", "关系");
-
+    ATTR("attr", "属性"), CI("ci", "模型"), REL("rel", "关系"),
+    CONST_ATTR("constattr", "内部属性");
     private final String value;
     private final String text;
 
