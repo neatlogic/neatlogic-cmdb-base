30d9f242bcb53b75c8101263aa3b3669e8935fcb
longrf <a5739119>
2022-06-08 10:11:14 +0800

    sql执行list返回添加accessEndpoint和nodeType

--- src/main/java/codedriver/framework/cmdb/crossover/ICiEntityCrossoverMapper.java
+++ src/main/java/codedriver/framework/cmdb/crossover/ICiEntityCrossoverMapper.java
@@ -30,4 +30,12 @@ public interface ICiEntityCrossoverMapper extends ICrossoverService {
      * @return 属性列表
      */
     List<AttrEntityVo> getAttrEntityByAttrIdAndFromCiEntityId(@Param("fromCiEntityId") Long fromCiEntityId, @Param("attrId") Long attrId, @Param("limit") Long limit);
+
+    /**
+     * 根据id列表返回多个配置项基本信息
+     *
+     * @param ciEntityIdList 配置项id列表
+     * @return 多个CiEntityVo
+     */
+    List<CiEntityVo> getCiEntityBaseInfoByIdList(@Param("ciEntityIdList") List<Long> ciEntityIdList);
 }
