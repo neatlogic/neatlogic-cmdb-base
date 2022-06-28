e2e59a086ee19800ceef47f82b565399b123cd0d
linbq@techsure.com.cn <Besiter789>
2022-06-28 09:51:06 +0800

    --story=1006509 --user=林邦泉 配置管理-资产清单查询响应速度优化 https://www.tapd.cn/54247054/s/1237584

--- src/main/java/codedriver/framework/cmdb/dto/resourcecenter/customview/ResourceDetailView.java
+++ src/main/java/codedriver/framework/cmdb/dto/resourcecenter/customview/ResourceDetailView.java
@@ -6,16 +6,11 @@
 package codedriver.framework.cmdb.dto.resourcecenter.customview;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
-import codedriver.framework.jsqlparser.OrExpressionList;
 import net.sf.jsqlparser.expression.*;
-import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
-import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.select.*;
-
-import java.util.Arrays;
 
 /**
  * @author linbq
@@ -181,23 +176,6 @@ public class ResourceDetailView implements ICustomView {
     }
 
     public static void main(String[] args) {
-        String fromTableName = "cmdb_" + 111;
-        String fromTableAlias = "resource_ipobject";
-        Table fromTable = new Table(fromTableName).withAlias(new Alias(fromTableAlias).withUseAs(false));
-        PlainSelect plainSelect = new PlainSelect()
-                .withFromItem(fromTable)
-                .addSelectItems(new SelectExpressionItem(new Function().withName("COUNT").withDistinct(true).withParameters(new ExpressionList(Arrays.asList(new Column(fromTable, "id"))))))
-                ;
-        ;
-//        InExpression inExpression = new InExpression(new Column(fromTable, "id"), new ExpressionList(Arrays.asList(new LongValue(10L))));
-//        plainSelect.setWhere(inExpression);
-        OrExpressionList expressionList = new OrExpressionList();
-        expressionList.addExpressions(new EqualsTo(new Column(new Table("a"), "b"), new StringValue("c")));
-        expressionList.addExpressions(new EqualsTo(new Column(new Table("d"), "e"), new StringValue("f")));
-        expressionList.addExpressions(new EqualsTo(new Column(new Table("g"), "h"), new StringValue("i")));
-        System.out.println(new AndExpression(new EqualsTo(new Column(new Table("a"), "b"), new StringValue("c")), expressionList));
-//        plainSelect.setWhere(expressionList);
-        System.out.println(plainSelect.toString());
 //        String sql = "select COUNT(DISTINCT a.`id`) as num, a.`id`, a.`name` from user a where a.`id` = ?";
 //        try {
 //            Statement statement = CCJSqlParserUtil.parse(sql);
