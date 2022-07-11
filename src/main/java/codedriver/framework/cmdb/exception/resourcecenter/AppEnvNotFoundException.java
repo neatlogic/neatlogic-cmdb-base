57f1776a4e7225142c9574d6c659f6f7ca5ec04b
赖文韬 <laiwt@techsure.com.cn>
2022-07-11 16:00:06 +0800

    自动发布对外接口-getIdPath

--- src/main/java/codedriver/framework/cmdb/exception/resourcecenter/AppEnvNotFoundException.java
+++ src/main/java/codedriver/framework/cmdb/exception/resourcecenter/AppEnvNotFoundException.java
@@ -11,15 +11,11 @@ import codedriver.framework.exception.core.ApiRuntimeException;
  * @author linbq
  * @since 2021/6/17 10:34
  **/
-public class AppModuleNotFoundException extends ApiRuntimeException {
+public class AppEnvNotFoundException extends ApiRuntimeException {
 
-    private static final long serialVersionUID = 8738518138638398100L;
+    private static final long serialVersionUID = -5353639287406720093L;
 
-    public AppModuleNotFoundException(Long id) {
-        super("应用模块：'" + id + "'不存在");
-    }
-
-    public AppModuleNotFoundException(String name) {
-        super("应用模块：'" + name + "'不存在");
+    public AppEnvNotFoundException(String name) {
+        super("环境：'" + name + "'不存在");
     }
 }
