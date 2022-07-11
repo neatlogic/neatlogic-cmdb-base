0250255c83af227dad30176403958f07f51cdaa1
linbq@techsure.com.cn <Besiter789>
2022-07-11 11:42:55 +0800

    --task=1006566 --user=林邦泉 CMDB资产清单性能优化-重构代码 https://www.tapd.cn/54247054/s/1240228

--- src/main/java/codedriver/framework/cmdb/crossover/IResourceCenterCommonGenerateSqlCrossoverService.java
+++ src/main/java/codedriver/framework/cmdb/crossover/IResourceCenterCommonGenerateSqlCrossoverService.java
@@ -20,90 +20,90 @@ public interface IResourceCenterCommonGenerateSqlCrossoverService extends ICross
 
     /**
      * 根据查询条件组装查询资源总个数的PlainSelect对象
-     * @param mainResourceId
-     * @param biConsumerList
+     * @param mainResourceId 视图名
+     * @param biConsumerList 过滤条件列表
      * @return
      */
     PlainSelect getResourceCountPlainSelect(String mainResourceId, List<BiConsumer<ResourceSearchGenerateSqlUtil, PlainSelect>> biConsumerList);
 
     /**
      * 根据查询条件组装查询资源总个数的sql语句
-     * @param mainResourceId
-     * @param biConsumerList
+     * @param mainResourceId 视图名
+     * @param biConsumerList 过滤条件列表
      * @return
      */
     String getResourceCountSql(String mainResourceId, List<BiConsumer<ResourceSearchGenerateSqlUtil, PlainSelect>> biConsumerList);
 
     /**
      * 根据查询条件组装查询当前页id列表的sql语句
-     * @param mainResourceId
-     * @param biConsumerList
-     * @param startNum
-     * @param pageSize
+     * @param mainResourceId 视图名
+     * @param biConsumerList 过滤条件列表
+     * @param startNum 分页开始偏移量
+     * @param pageSize 页大小
      * @return
      */
     String getResourceIdListSql(String mainResourceId, List<BiConsumer<ResourceSearchGenerateSqlUtil, PlainSelect>> biConsumerList, int startNum, int pageSize);
 
     /**
      * 根据查询条件组装查询当前页id列表的sql语句
-     * @param plainSelect
+     * @param plainSelect sql语句
      * @return
      */
     String getResourceIdListSql(PlainSelect plainSelect);
 
     /**
      * 根据查询条件组装查询当前页id列表的sql语句
-     * @param plainSelect
-     * @param startNum
-     * @param pageSize
+     * @param plainSelect sql语句
+     * @param startNum 分页开始偏移量
+     * @param pageSize 页大小
      * @return
      */
     String getResourceIdListSql(PlainSelect plainSelect, int startNum, int pageSize);
 
     /**
      * 根据查询条件组装查询只返回一个id的sql语句
-     * @param mainResourceId
-     * @param biConsumerList
+     * @param mainResourceId 视图名
+     * @param biConsumerList 过滤条件列表
      * @return
      */
     String getResourceIdSql(String mainResourceId, List<BiConsumer<ResourceSearchGenerateSqlUtil, PlainSelect>> biConsumerList);
 
     /**
      * 根据需要查询的列，生成对应的sql语句
-     * @param plainSelect
-     * @param theadList
-     * @param unavailableResourceInfoList
+     * @param plainSelect sql语句
+     * @param theadList 需要查询数据字段列表
+     * @param unavailableResourceInfoList 资源中心数据初始化配置中没有定义的字段集合
      * @return
      */
     String getResourceListSql(PlainSelect plainSelect, List<ResourceInfo> theadList, List<ResourceInfo> unavailableResourceInfoList);
 
     /**
      * 根据需要查询的列，生成对应的sql语句
-     * @param mainResourceId
-     * @param theadList
-     * @param idList
-     * @param unavailableResourceInfoList
+     * @param mainResourceId 视图名
+     * @param theadList 需要查询数据字段列表
+     * @param idList id列表
+     * @param unavailableResourceInfoList 资源中心数据初始化配置中没有定义的字段集合
      * @return
      */
     String getResourceListByIdListSql(String mainResourceId, List<ResourceInfo> theadList, List<Long> idList, List<ResourceInfo> unavailableResourceInfoList);
 
     /**
      * 根据需要查询的列和查询条件，生成对应的sql语句执行，返回ResourceVo列表
-     * @param mainResourceId
-     * @param theadList
-     * @param biConsumerList
-     * @param basePageVo
-     * @param unavailableResourceInfoList
+     * @param mainResourceId 视图名
+     * @param theadList 需要查询数据字段列表
+     * @param biConsumerList 过滤条件列表
+     * @param basePageVo 分页信息
+     * @param unavailableResourceInfoList 资源中心数据初始化配置中没有定义的字段集合
      * @return
      */
     List<ResourceVo> getResourceList(String mainResourceId, List<ResourceInfo> theadList, List<BiConsumer<ResourceSearchGenerateSqlUtil, PlainSelect>> biConsumerList, BasePageVo basePageVo, List<ResourceInfo> unavailableResourceInfoList);
 
     /**
      * 根据需要查询的列和查询条件，生成对应的sql语句
-     * @param mainResourceId
-     * @param theadList
-     * @param biConsumerList
-     * @param unavailableResourceInfoList
+     * @param mainResourceId 视图名
+     * @param theadList 需要查询数据字段列表
+     * @param biConsumerList 过滤条件列表
+     * @param unavailableResourceInfoList 资源中心数据初始化配置中没有定义的字段集合
      * @return
      */
     String getResourceListSql(
