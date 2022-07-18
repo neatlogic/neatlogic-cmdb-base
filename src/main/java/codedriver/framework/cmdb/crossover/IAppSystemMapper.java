ffc4c4fdb64a06b1352ecacbadbb500436688d5c
lvzk <lvzk@techsure.com.cn>
2022-07-18 19:57:04 +0800

    update deploy job

--- src/main/java/codedriver/framework/cmdb/crossover/IAppSystemMapper.java
+++ src/main/java/codedriver/framework/cmdb/crossover/IAppSystemMapper.java
@@ -7,12 +7,18 @@ package codedriver.framework.cmdb.crossover;
 
 import codedriver.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
 import codedriver.framework.cmdb.dto.resourcecenter.entity.AppEnvironmentVo;
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppModuleVo;
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppSystemVo;
 import codedriver.framework.crossover.ICrossoverService;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface IAppSystemMapper extends ICrossoverService {
+    AppSystemVo getAppSystemByAbbrName(@Param("abbrName")String abbrName, @Param("schemaName") String schemaName);
+
+    AppModuleVo getAppModuleByAbbrName(@Param("abbrName")String abbrName, @Param("schemaName") String schemaName);
+
     List<AppEnvironmentVo> getAppEnvListByAppSystemIdAndModuleIdList(@Param("appResourceId") Long appResourceId, @Param("moduleResourceIdList") List<Long> moduleIdList, @Param("schemaName") String schemaName);
 
     Integer getAppSystemIdListCount(ResourceSearchVo searchVo);
