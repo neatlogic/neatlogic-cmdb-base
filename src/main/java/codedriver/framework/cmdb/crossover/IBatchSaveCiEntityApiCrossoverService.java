3b196ec14fe2f542f4bb42b1821e42e76e53c9e5
lvzk <lvzk@techsure.com.cn>
2022-07-28 14:27:26 +0800

    update batch save ci entity  移到苏州银行模块

--- src/main/java/codedriver/framework/cmdb/crossover/IBatchSaveCiEntityApiCrossoverService.java
+++ src/main/java/codedriver/framework/cmdb/crossover/IBatchSaveCiEntityApiCrossoverService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * Copyright (c)  2022 TechSure Co.,Ltd.  All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -8,7 +8,7 @@ package codedriver.framework.cmdb.crossover;
 import codedriver.framework.crossover.ICrossoverService;
 import com.alibaba.fastjson.JSONObject;
 
-public interface IBatchSaveCiEntityPublicApiCrossoverService extends ICrossoverService {
+public interface IBatchSaveCiEntityApiCrossoverService extends ICrossoverService {
 
     Object myDoService(JSONObject jsonObj) throws Exception;
 
