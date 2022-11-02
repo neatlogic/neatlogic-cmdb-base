f3a1675b166af81453a0fac844947e17a645eced
linbq@techsure.com.cn <Besiter789>
2022-11-02 16:46:47 +0800

    --task=1007094 --user=林邦泉 后端-资源中心-应用清单数据显方式修改&增加操作系统信息 https://www.tapd.cn/54247054/s/1259732

---
+++ src/main/java/codedriver/framework/cmdb/dto/resourcecenter/AppModuleVo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.cmdb.dto.resourcecenter;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
+import java.io.Serializable;
+
+public class AppModuleVo implements Serializable {
+    private static final long serialVersionUID = 3794860793337591802L;
+
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "简称", type = ApiParamType.STRING)
+    private String abbrName;
+    @EntityField(name = "应用id", type = ApiParamType.LONG)
+    private Long appSystemId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbrName() {
+        return abbrName;
+    }
+
+    public void setAbbrName(String abbrName) {
+        this.abbrName = abbrName;
+    }
+
+    public Long getAppSystemId() {
+        return appSystemId;
+    }
+
+    public void setAppSystemId(Long appSystemId) {
+        this.appSystemId = appSystemId;
+    }
+}
