98cdbcd34572c21f2b491a1ad9540bfdcd7c9af6
吕佐康 <lvzk@techsure.com.cn>
2022-12-30 17:08:12 +0800

    --story=1007143 --user=lvzk 自动化 -- 执行目标选择（包括所有入口）-- 过滤器方式 --增加【高级模式】选项 https://www.tapd.cn/54247054/s/1264750

--- src/main/java/codedriver/framework/cmdb/resourcecenter/condition/IResourcecenterCondition.java
+++ src/main/java/codedriver/framework/cmdb/resourcecenter/condition/IResourcecenterCondition.java
@@ -5,12 +5,37 @@
 
 package codedriver.framework.cmdb.resourcecenter.condition;
 
-import codedriver.framework.condition.core.IConditionHandler;
 import codedriver.framework.dto.condition.ConditionVo;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.List;
 
-public interface IResourcecenterCondition extends IConditionHandler {
+public interface IResourcecenterCondition {
+    /**
+     * 条件英文名
+     * @return 条件英文名
+     */
+    String getName();
 
+    /**
+     * 条件显示名
+     * @return 条件显示名
+     */
+    String getDisplayName();
+
+    /**
+     * 根据值获取值的text
+     * @param value 值
+     * @param config 配置
+     * @return text
+     */
+    Object valueConversionText(Object value, JSONObject config);
+
+    /**
+     * 获取动态where sql
+     * @param conditionList 条件
+     * @param index 当前条件下标
+     * @param sqlSb 动态where sql
+     */
     void getSqlConditionWhere(List<ConditionVo> conditionList, Integer index, StringBuilder sqlSb);
 }
--- src/main/java/codedriver/framework/cmdb/resourcecenter/condition/ResourcecenterConditionBase.java
+++ src/main/java/codedriver/framework/cmdb/resourcecenter/condition/ResourcecenterConditionBase.java
@@ -5,34 +5,14 @@
 
 package codedriver.framework.cmdb.resourcecenter.condition;
 
-import codedriver.framework.cmdb.enums.resourcecenter.condition.ConditionConfigType;
 import codedriver.framework.common.constvalue.Expression;
 import codedriver.framework.dto.condition.ConditionVo;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
 public abstract class ResourcecenterConditionBase implements IResourcecenterCondition{
-
-    @Override
-    public JSONObject getConfig(Enum<?> type) {
-        if (type instanceof ConditionConfigType) {
-            ConditionConfigType configType = (ConditionConfigType) type;
-            return getConfig(configType);
-        } else {
-            return getConfig(ConditionConfigType.DEFAULT);
-        }
-    }
-
-    @Override
-    public JSONObject getConfig() {
-        return getConfig(ConditionConfigType.DEFAULT);
-    }
-
-    public abstract JSONObject getConfig(ConditionConfigType type);
-
 
     protected void getSimpleSqlConditionWhere(ConditionVo condition, StringBuilder sqlSb, String tableShortName, String columnName) {
         Object value = StringUtils.EMPTY;
