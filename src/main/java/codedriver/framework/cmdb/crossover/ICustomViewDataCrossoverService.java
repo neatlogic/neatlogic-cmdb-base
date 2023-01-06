9c7fa4004902f5d2597de04c74861f12b86f26bd
陈其炜 <chenqw@techsure.com.cn>
2023-01-06 18:13:10 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/cmdb/crossover/ICustomViewDataCrossoverService.java
+++ src/main/java/codedriver/framework/cmdb/crossover/ICustomViewDataCrossoverService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -12,5 +12,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface ICustomViewDataCrossoverService extends ICrossoverService {
-    List<Map<String, Object>> searchCustomViewData(CustomViewConditionVo customViewConditionVo);
+
+    List<Map<String, Object>> searchCustomViewDataFlatten(CustomViewConditionVo customViewConditionVo);
 }
