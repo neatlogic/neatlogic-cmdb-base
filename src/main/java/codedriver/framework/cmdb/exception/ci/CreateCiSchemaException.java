43291b0c5ff80d8fa1e8482c15b58bc672716b64
陈其炜 <chenqw@techsure.com.cn>
2023-01-10 17:15:33 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/cmdb/exception/ci/CreateCiSchemaException.java
+++ src/main/java/codedriver/framework/cmdb/exception/ci/CreateCiSchemaException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -9,10 +9,10 @@ import codedriver.framework.exception.core.ApiRuntimeException;
 
 public class CreateCiSchemaException extends ApiRuntimeException {
     public CreateCiSchemaException(String ciName) {
-        super("创建模型：" + ciName + "数据表失败。");
+        super("创建模型“" + ciName + "”数据表失败");
     }
 
     public CreateCiSchemaException(String ciName, boolean isView) {
-        super("创建模型：" + ciName + "数据视图失败。");
+        super("创建模型“" + ciName + "”数据视图失败");
     }
 }
