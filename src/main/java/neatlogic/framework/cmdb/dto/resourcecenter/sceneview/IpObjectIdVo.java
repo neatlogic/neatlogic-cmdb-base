b12d8ac7a4ba104493fce9fd6f2d238ff8e73f3e
linbangquan <1437892690@qq.com>
2023-08-14 15:04:21 +0800

    [功能] 场景视图字段固化到代码中
    关联 #[957880973361152]场景视图字段固化到代码中 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/957880973361152

---
+++ src/main/java/neatlogic/framework/cmdb/dto/resourcecenter/sceneview/IpObjectIdVo.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.framework.cmdb.dto.resourcecenter.sceneview;
+
+import neatlogic.framework.cmdb.annotation.ResourceField;
+import neatlogic.framework.cmdb.annotation.ResourceType;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.EntityField;
+
+@ResourceType(name = "scence_ipobject_id", label = "IP软硬件ID场景")
+public class IpObjectIdVo {
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    @ResourceField(name = "id")
+    private Long id;
+
+    @EntityField(name = "类型id", type = ApiParamType.LONG)
+    @ResourceField(name = "type_id")
+    private Long typeId;
+    @EntityField(name = "类型名称", type = ApiParamType.STRING)
+    @ResourceField(name = "type_name")
+    private String typeName;
+    @EntityField(name = "类型label", type = ApiParamType.STRING)
+    @ResourceField(name = "type_label")
+    private String typeLabel;
+}
---
+++ src/main/java/neatlogic/framework/cmdb/dto/resourcecenter/sceneview/IpObjectIpPortVo.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.framework.cmdb.dto.resourcecenter.sceneview;
+
+import neatlogic.framework.cmdb.annotation.ResourceField;
+import neatlogic.framework.cmdb.annotation.ResourceType;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.EntityField;
+
+@ResourceType(name = "scence_ipobject_ip_port", label = "IP软硬件IP和端口场景")
+public class IpObjectIpPortVo {
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    @ResourceField(name = "id")
+    private Long id;
+
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    @ResourceField(name = "name")
+    private String name;
+
+    @EntityField(name = "类型id", type = ApiParamType.LONG)
+    @ResourceField(name = "type_id")
+    private Long typeId;
+    @EntityField(name = "类型名称", type = ApiParamType.STRING)
+    @ResourceField(name = "type_name")
+    private String typeName;
+    @EntityField(name = "类型label", type = ApiParamType.STRING)
+    @ResourceField(name = "type_label")
+    private String typeLabel;
+
+    @EntityField(name = "IP地址", type = ApiParamType.STRING)
+    @ResourceField(name = "ip")
+    private String ip;
+
+    @EntityField(name = "端口", type = ApiParamType.INTEGER)
+    @ResourceField(name = "port")
+    private Integer port;
+}
