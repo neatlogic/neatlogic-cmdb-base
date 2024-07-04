f0aba30b777c41c4e6ae438796ed295fb7286862
lvzk <897706680@qq.com>
2024-07-04 15:54:40 +0800

    [功能]作业优化

--- src/main/java/neatlogic/framework/cmdb/exception/resourcecenter/ResourceCenterAccountProtocolNotFoundException.java
+++ src/main/java/neatlogic/framework/cmdb/exception/resourcecenter/ResourceCenterAccountProtocolNotFoundException.java
@@ -6,10 +6,10 @@ public class ResourceCenterAccountProtocolNotFoundException extends ApiRuntimeEx
     private static final long serialVersionUID = -5055024535330898128L;
 
     public ResourceCenterAccountProtocolNotFoundException(Long id) {
-        super("协议：{0}不存在", id);
+        super("nfcer.resourcecenteraccountprotocolnotfoundexception.resourcecenteraccountprotocolnotfoundexception.id", id);
     }
 
     public ResourceCenterAccountProtocolNotFoundException(String name) {
-        super("协议：{0}不存在", name);
+        super("nfcer.resourcecenteraccountprotocolnotfoundexception.resourcecenteraccountprotocolnotfoundexception.id", name);
     }
 }
