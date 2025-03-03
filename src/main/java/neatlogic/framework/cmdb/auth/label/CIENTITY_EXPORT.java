c190a2a3575c7096482c3b6f4e0d81edaac06f6f
chenqw <gdnpc@163.com>
2025-03-03 14:58:44 +0800

    -[功能]对配置项导出增加权限控制
    Signed-off-by: chenqw <gdnpc@163.com>

---
+++ src/main/java/neatlogic/framework/cmdb/auth/label/CIENTITY_EXPORT.java
@@ -0,0 +1,49 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.cmdb.auth.label;
+
+import neatlogic.framework.auth.core.AuthBase;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CIENTITY_EXPORT extends AuthBase {
+
+    @Override
+    public String getAuthDisplayName() {
+        return "导出配置项列表权限";
+    }
+
+    @Override
+    public String getAuthIntroduction() {
+        return "对配置项数据进行导出";
+    }
+
+    @Override
+    public String getAuthGroup() {
+        return "cmdb";
+    }
+
+    @Override
+    public Integer getSort() {
+        return 5;
+    }
+
+    @Override
+    public List<Class<? extends AuthBase>> getIncludeAuths() {
+        return Collections.singletonList(CMDB_BASE.class);
+    }
+}
