791c33de2ba3e70ca111c57fc78508123f6d4506
1437892690@qq.com <1437892690@qq.com>
2025-04-27 00:00:05 +0800

    [功能] tagent注册新增os资产ip视图
    关联 #[1407326936858624]tagent注册新增os资产ip视图 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1407326936858624

---
+++ src/main/java/neatlogic/framework/cmdb/dto/resourcecenter/sceneviewfielddeclare/OSVo.java
@@ -0,0 +1,47 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.cmdb.dto.resourcecenter.sceneviewfielddeclare;
+
+import neatlogic.framework.cmdb.annotation.ResourceField;
+import neatlogic.framework.cmdb.annotation.ResourceType;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.EntityField;
+
+@ResourceType(name = "scence_os", label = "操作系统IP视图", moduleId= "cmdb", functionPathList = {"配置管理/添加tagent数据进配置项"})
+public class OSVo {
+    @EntityField(name = "ID", type = ApiParamType.LONG)
+    @ResourceField(name = "id")
+    private Long id;
+
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    @ResourceField(name = "name")
+    private String name;
+
+    @EntityField(name = "IP地址", type = ApiParamType.STRING)
+    @ResourceField(name = "ip")
+    private String ip;
+
+    @EntityField(name = "类型ID", type = ApiParamType.LONG)
+    @ResourceField(name = "type_id")
+    private Long typeId;
+    @EntityField(name = "类型名称", type = ApiParamType.STRING)
+    @ResourceField(name = "type_name")
+    private String typeName;
+    @EntityField(name = "类型Label", type = ApiParamType.STRING)
+    @ResourceField(name = "type_label")
+    private String typeLabel;
+
+}
