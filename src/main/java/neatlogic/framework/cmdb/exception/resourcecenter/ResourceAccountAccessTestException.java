0a648fb8ad078f69c86235bb96e4824ed5e5f1b4
lvzk <897706680@qq.com>
2025-08-06 16:48:38 +0800

    [功能]测试账号支持域名解析 #[1481192128348160]测试账号支持域名解析 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1481192128348160

--- src/main/java/neatlogic/framework/cmdb/exception/resourcecenter/ResourceAccountAccessTestException.java
+++ src/main/java/neatlogic/framework/cmdb/exception/resourcecenter/ResourceAccountAccessTestException.java
@@ -4,6 +4,8 @@ import neatlogic.framework.exception.core.ApiRuntimeException;
 
 public class ResourceAccountAccessTestException extends ApiRuntimeException {
 
+    private static final long serialVersionUID = 3429631542042868737L;
+
     public ResourceAccountAccessTestException(String msg) {
         super(msg);
     }
