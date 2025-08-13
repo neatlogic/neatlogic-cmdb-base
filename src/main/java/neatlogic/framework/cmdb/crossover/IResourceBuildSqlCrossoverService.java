d3166dd2051d1d6ea507fa8dedae6a343c6aa807
1437892690@qq.com <1437892690@qq.com>
2025-08-13 15:50:49 +0800

    [功能] 将涉及scence_ipobject_detail视图的SQL语句改成动态sql
    关联 #[1469827334504448]将涉及scence_ipobject_detail视图的SQL语句改成动态sql http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1469827334504448

---
+++ src/main/java/neatlogic/framework/cmdb/crossover/IResourceBuildSqlCrossoverService.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2025  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.framework.cmdb.crossover;
+
+import neatlogic.framework.cmdb.dto.resourcecenter.AccountComponentVo;
+import neatlogic.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
+import neatlogic.framework.cmdb.dto.resourcecenter.ResourceVo;
+import neatlogic.framework.crossover.ICrossoverService;
+
+import java.util.List;
+
+public interface IResourceBuildSqlCrossoverService extends ICrossoverService {
+
+    String buildGetResourceIdListSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceCountSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceListSql(List<Long> idList, List<String> selectFieldNameList);
+
+    String buildGetResourceListSql(List<Long> idList);
+
+    String buildGetResourceCountByNameKeywordSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceCountByIpKeywordSql(ResourceSearchVo searchVo);
+
+    String buildGetAuthResourceListSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceListByIpAndPortAndNameWithFilterSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceTypeIdListByAuthSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceIdByIpAndPortAndNameSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceIdListByIpAndPortAndNameSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceListByIpAndPortAndNameSql(ResourceSearchVo searchVo);
+
+    String buildGetResourceByIdListSql(List<Long> idList);
+
+    String buildGetResourceByIdSql(Long id, List<String> selectFieldNameList);
+
+    String buildGetResourceByIdSql(Long id);
+
+    String buildGetResourceIdByResourceIdSql(Long id);
+
+    String buildCheckResourceIdListIsExistsSql(List<Long> idList);
+
+    String buildGetResourceIdListByAppSystemIdAndModuleIdAndEnvIdSql(ResourceVo resourceVo);
+
+    String buildGetResourceListByTypeIdListAndIpListSql(List<Long> typeIdList, List<String> ipList);
+
+    String buildGetResourceByIpAndPortAndNameAndTypeNameSql(String ip, Integer port, String name, String typeName);
+
+    String buildGetResourceByIpAndPortSql(String ip, Integer port);
+
+    String buildSearchAccountComponentSql(AccountComponentVo accountComponentVo);
+
+    String buildSearchAccountComponentCountSql(AccountComponentVo accountComponentVo);
+
+    String buildGetAppEnvListByAppSystemIdAndAppModuleIdSql(Long appSystemId, Long appModuleId);
+
+    String buildGetAppEnvCountMapByAppSystemIdGroupByAppModuleIdSql(Long appSystemId);
+
+//    String buildGetResourceCountByDynamicConditionSql();
+//    String buildGetResourceIdListByDynamicConditionSql();
+
+    // InspectMapper
+    String buildGetInspectResourceListByIdListSql(List<Long> idList, List<String> selectFieldNameList);
+
+    String buildGetInspectResourceListByIdListSql(List<Long> idList);
+
+    String buildGetInspectResourceCountSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectResourceCountByIpKeywordSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectResourceCountByNameKeywordSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectResourceIdListSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectAutoexecJobNodeResourceCountSql(ResourceSearchVo searchVo, Long jobId);
+
+    String buildGetInspectAutoexecJobNodeResourceCountByIpKeywordSql(ResourceSearchVo searchVo, Long jobId);
+
+    String buildGetInspectAutoexecJobNodeResourceCountByNameKeywordSql(ResourceSearchVo searchVo, Long jobId);
+
+    String buildGetInspectAutoexecJobNodeResourceIdListSql(ResourceSearchVo searchVo, Long jobId);
+
+    String buildGetInspectResourceListByIdListAndJobIdSql(List<Long> IdList, Long jobId);
+
+    String buildGetInspectResourceListByIdListAndJobIdSql(List<Long> IdList, Long jobId, List<String> selectFieldNameList);
+
+    // InspectConfigFileMapper
+    String buildGetInspectConfigFileResourceIdListSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectConfigFilePathCountSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectConfigFilePathIdListSql(ResourceSearchVo searchVo);
+
+    String buildGetInspectConfigFilePathListSql(List<Long> idList);
+
+    String buildGetInspectConfigFilePathListByJobIdSql(Long jobId);
+}
--- src/main/java/neatlogic/framework/cmdb/crossover/IResourceCenterResourceCrossoverService.java
+++ src/main/java/neatlogic/framework/cmdb/crossover/IResourceCenterResourceCrossoverService.java
@@ -16,10 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 package neatlogic.framework.cmdb.crossover;
 
 import com.alibaba.fastjson.JSONObject;
-import neatlogic.framework.cmdb.dto.resourcecenter.AccountComponentVo;
 import neatlogic.framework.cmdb.dto.resourcecenter.AccountVo;
 import neatlogic.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
-import neatlogic.framework.cmdb.dto.resourcecenter.ResourceVo;
 import neatlogic.framework.cmdb.dto.tag.TagVo;
 import neatlogic.framework.crossover.ICrossoverService;
 
@@ -68,95 +66,6 @@ public interface IResourceCenterResourceCrossoverService extends ICrossoverServi
      * @return map<资产id ， 标签列表>
      */
     Map<Long, List<TagVo>> getResourceTagByResourceIdList(List<Long> idList);
-
-    String buildGetResourceIdListSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceCountSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceListSql(List<Long> idList, List<String> selectFieldNameList);
-
-    String buildGetResourceListSql(List<Long> idList);
-
-    String buildGetResourceCountByNameKeywordSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceCountByIpKeywordSql(ResourceSearchVo searchVo);
-
-    String buildGetAuthResourceListSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceListByIpAndPortAndNameWithFilterSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceTypeIdListByAuthSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceIdByIpAndPortAndNameSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceIdListByIpAndPortAndNameSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceListByIpAndPortAndNameSql(ResourceSearchVo searchVo);
-
-    String buildGetResourceByIdListSql(List<Long> idList);
-
-    String buildGetResourceByIdSql(Long id, List<String> selectFieldNameList);
-
-    String buildGetResourceByIdSql(Long id);
-
-    String buildGetResourceIdByResourceIdSql(Long id);
-
-    String buildCheckResourceIdListIsExistsSql(List<Long> idList);
-
-    String buildGetResourceIdListByAppSystemIdAndModuleIdAndEnvIdSql(ResourceVo resourceVo);
-
-    String buildGetResourceListByTypeIdListAndIpListSql(List<Long> typeIdList, List<String> ipList);
-
-    String buildGetResourceByIpAndPortAndNameAndTypeNameSql(String ip, Integer port, String name, String typeName);
-
-    String buildGetResourceByIpAndPortSql(String ip, Integer port);
-
-    String buildSearchAccountComponentSql(AccountComponentVo accountComponentVo);
-
-    String buildSearchAccountComponentCountSql(AccountComponentVo accountComponentVo);
-
-    String buildGetAppEnvListByAppSystemIdAndAppModuleIdSql(Long appSystemId, Long appModuleId);
-
-    String buildGetAppEnvCountMapByAppSystemIdGroupByAppModuleIdSql(Long appSystemId);
-
-//    String buildGetResourceCountByDynamicConditionSql();
-//    String buildGetResourceIdListByDynamicConditionSql();
-
-    // InspectMapper
-    String buildGetInspectResourceListByIdListSql(List<Long> idList, List<String> selectFieldNameList);
-
-    String buildGetInspectResourceListByIdListSql(List<Long> idList);
-
-    String buildGetInspectResourceCountSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectResourceCountByIpKeywordSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectResourceCountByNameKeywordSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectResourceIdListSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectAutoexecJobNodeResourceCountSql(ResourceSearchVo searchVo, Long jobId);
-
-    String buildGetInspectAutoexecJobNodeResourceCountByIpKeywordSql(ResourceSearchVo searchVo, Long jobId);
-
-    String buildGetInspectAutoexecJobNodeResourceCountByNameKeywordSql(ResourceSearchVo searchVo, Long jobId);
-
-    String buildGetInspectAutoexecJobNodeResourceIdListSql(ResourceSearchVo searchVo, Long jobId);
-
-    String buildGetInspectResourceListByIdListAndJobIdSql(List<Long> IdList, Long jobId);
-
-    String buildGetInspectResourceListByIdListAndJobIdSql(List<Long> IdList, Long jobId, List<String> selectFieldNameList);
-
-    // InspectConfigFileMapper
-    String buildGetInspectConfigFileResourceIdListSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectConfigFilePathCountSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectConfigFilePathIdListSql(ResourceSearchVo searchVo);
-
-    String buildGetInspectConfigFilePathListSql(List<Long> idList);
-
-    String buildGetInspectConfigFilePathListByJobIdSql(Long jobId);
 
 }
 
