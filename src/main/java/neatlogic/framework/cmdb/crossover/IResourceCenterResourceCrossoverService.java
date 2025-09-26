68f3d96a09f26e738744df53a6d362bd19fdf0c3
linbangquan <1437892690@qq.com>
2025-09-26 02:37:35 +0000

    !56 [功能] 将涉及scence_vendor视图的SQL语句改成动态sql * [功能] 将涉及scence_vendor视图的SQL语句改成动态sql * [功能] 将涉及scence_ipobject_detail视图的SQL语句改成动态sql * [功能] 将涉及scence_ipobject_detail视图的SQL语句改成动态sql

--- src/main/java/neatlogic/framework/cmdb/crossover/IResourceCenterResourceCrossoverService.java
+++ src/main/java/neatlogic/framework/cmdb/crossover/IResourceCenterResourceCrossoverService.java
@@ -18,6 +18,7 @@ package neatlogic.framework.cmdb.crossover;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.cmdb.dto.resourcecenter.*;
 import neatlogic.framework.cmdb.dto.tag.TagVo;
+import neatlogic.framework.common.dto.BasePageVo;
 import neatlogic.framework.crossover.ICrossoverService;
 
 import java.util.List;
@@ -123,6 +124,12 @@ public interface IResourceCenterResourceCrossoverService extends ICrossoverServi
     );
 
     List<Long> getAppSystemIdListById(String viewName, Long id);
+
+    int searchVendorCount(BasePageVo searchVo);
+
+    List<Long> searchVendorIdList(BasePageVo searchVo);
+
+    List<ResourceVo> searchVendorListByIdList(List<Long> idList);
 
 }
 
