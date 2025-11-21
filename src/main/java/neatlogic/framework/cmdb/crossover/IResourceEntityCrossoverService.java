356e296402b63d1ec88232095d3861c99c2f328e
1437892690@qq.com <1437892690@qq.com>
2025-11-21 16:45:32 +0800

    [修复] 资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点
    关联 #[1559494255935488]资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1559494255935488

--- src/main/java/neatlogic/framework/cmdb/crossover/IResourceEntityCrossoverService.java
+++ src/main/java/neatlogic/framework/cmdb/crossover/IResourceEntityCrossoverService.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
  * This file is part of the NeatLogic software.
  * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
@@ -7,15 +6,16 @@
  * See the LICENSE file distributed with this work for the full license text.
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
  */
 
 package neatlogic.framework.cmdb.crossover;
 
-import neatlogic.framework.cmdb.dto.resourcecenter.config.ResourceEntityVo;
+import neatlogic.framework.cmdb.dto.ci.CiVo;
 import neatlogic.framework.crossover.ICrossoverService;
 
-public interface IResourceEntityCrossoverMapper extends ICrossoverService {
+public interface IResourceEntityCrossoverService extends ICrossoverService {
 
-    ResourceEntityVo getResourceEntityByName(String name);
+    CiVo getAssetListRootCi();
+
+    CiVo getViewRootCi(String viewName);
 }
