2d92b5d59db299956428186f5cc241871329f729
1437892690@qq.com <1437892690@qq.com>
2025-12-02 18:01:24 +0800

    [修复] 资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点
    关联 #[1559494255935488]资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1559494255935488

--- src/main/java/neatlogic/framework/cmdb/crossover/IResourceEntityCrossoverMapper.java
+++ src/main/java/neatlogic/framework/cmdb/crossover/IResourceEntityCrossoverMapper.java
@@ -18,8 +18,6 @@ package neatlogic.framework.cmdb.crossover;
 import neatlogic.framework.cmdb.dto.resourcecenter.config.ResourceEntityVo;
 import neatlogic.framework.crossover.ICrossoverService;
 
-import java.util.List;
-
 public interface IResourceEntityCrossoverMapper extends ICrossoverService {
 
     ResourceEntityVo getResourceEntityByName(String name);
