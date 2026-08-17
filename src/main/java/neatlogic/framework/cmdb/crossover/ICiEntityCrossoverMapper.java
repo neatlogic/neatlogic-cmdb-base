dd60c2d90ca4308975d7147b5d8b61e4a6044cbb
1437892690@qq.com <1437892690@qq.com>
2026-08-17 14:55:28 +0800

    [功能] 后端-机房管理
    关联 #[1729096900673543]后端-机房管理 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1729096900673543

--- src/main/java/neatlogic/framework/cmdb/crossover/ICiEntityCrossoverMapper.java
+++ src/main/java/neatlogic/framework/cmdb/crossover/ICiEntityCrossoverMapper.java
@@ -6,6 +6,7 @@ import neatlogic.framework.crossover.ICrossoverService;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author longrf
@@ -76,4 +77,6 @@ public interface ICiEntityCrossoverMapper extends ICrossoverService {
     List<CiEntityVo> getCiEntityListByCiIdListAndName(CiEntityVo ciEntityVo);
 
     Long getCiEntityIdByCiIdAndAttrIdAndAttrValue(@Param("ciId") Long ciId, @Param("attrId") Long attrId, @Param("attrValue") String attrValue);
+
+    List<Map<String, Object>> getCiEntityIdAndAttrValueByCiIdAndAttrIdAndKeyword(@Param("ciId") Long ciId, @Param("attrId") Long id, @Param("keyword") String keyword);
 }
