009eca3da13390fd60e664093f32f7d66c2295ea
1437892690@qq.com <1437892690@qq.com>
2026-09-22 08:04:11 +0800

    [功能] 后端-机房管理
    关联 #[1729096900673543]后端-机房管理 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1729096900673543

--- src/main/java/neatlogic/framework/cmdb/dto/cientity/AttrFilterVo.java
+++ src/main/java/neatlogic/framework/cmdb/dto/cientity/AttrFilterVo.java
@@ -164,6 +164,19 @@ public class AttrFilterVo implements Serializable {
 
     public void setValueList(JSONArray valueList) {
         this.valueList = valueList;
+    }/**
+     * 兼容现有Java调用方以List设置普通属性过滤值，统一转换为JSONArray保存。
+     *
+     * @param valueList 普通属性过滤值
+     */
+    @JSONField(serialize = false, deserialize = false)
+    public void setValueList(List<?> valueList) {
+        if (valueList == null) {
+            this.valueList = null;
+            return;
+        }
+        this.valueList = new JSONArray();
+        this.valueList.addAll(valueList);
     }
 
     public String getExpressionName() {
